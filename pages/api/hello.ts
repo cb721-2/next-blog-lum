cdb8092e1c8dbadc9cd00083ef203a59dc29b806
Clint Brodar <clint@lum.fm>
2021-10-14 16:39:34 -0700

    remove unused code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,9 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const randomElement = Math.floor(Math.random() * 8) + 1;
-  
-  const data = await (await fetch(`https://avatar-api-cb.herokuapp.com/api/characters/all?element=${randomElement}`)).json()
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let randomElement = Math.floor(Math.random() * 8) + 1;
+  if (randomElement === 7) {
+    randomElement = 1;
+  }
+  const data = await (
+    await fetch(
+      `https://avatar-api-cb.herokuapp.com/api/characters/all?element=${randomElement}`
+    )
+  ).json();
 
   res.status(200).json({ data });
 }
